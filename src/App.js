9e1cf76fe1a6f1bdc1b823e38a9a9cd80c104f56
oezlemcelik <155397894+oezlemcelik@users.noreply.github.com>
2024-06-28 14:59:48 +0200

    Added  bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>
-        Weather App
-      </h1>
+      <div className='container'>
+
+      <h1> Weather App </h1>
       <footer>
     This project was coded by <a href='https://github.com/oezlemcelik?tab=repositories'>Özlem Celik</a> and is <a href="https://github.com/oezlemcelik/weather-react-appjs" 
       target='_blank' rel='noreferrer'> open-sourced on Github</a>
       </footer>
+      </div>
     </div>
   );
 }
