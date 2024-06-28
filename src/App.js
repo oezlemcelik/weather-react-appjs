810b9ec3c1cf0dd853a09b74663245a8bf19cc4e
oezlemcelik <155397894+oezlemcelik@users.noreply.github.com>
2024-06-28 14:41:44 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
       <h1>
         Weather App
       </h1>
+      <footer>
+    This project was coded by <a href='https://github.com/oezlemcelik?tab=repositories'>Özlem Celik</a> and is <a href="https://github.com/oezlemcelik/weather-react-appjs" 
+      target='_blank' rel='noreferrer'> open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
