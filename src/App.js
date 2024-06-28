286904a9df8564fe0c80406a2baef9429b8c8656
oezlemcelik <155397894+oezlemcelik@users.noreply.github.com>
2024-06-28 16:11:11 +0200

    Added html to weather.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
+      
       <div className='container'>
+      <Weather />
 
-      <h1> Weather App </h1>
       <footer>
     This project was coded by <a href='https://github.com/oezlemcelik?tab=repositories'>Özlem Celik</a> and is <a href="https://github.com/oezlemcelik/weather-react-appjs" 
       target='_blank' rel='noreferrer'> open-sourced on Github</a>
